09cefe789ea703d652e26aa943026342632b3bb4
{Anwar <anwarelghazi8596@gmail.com>
2024-04-17 18:07:14 +0200

    done

--- index.js
+++ index.js
@@ -1,2 +1,6 @@
 function addingEventListener() {
+    let button = document.getElementById('button');
+    button.addEventListener('click', function(event) {
+      console.log('Button clicked!');
+    });
 }
